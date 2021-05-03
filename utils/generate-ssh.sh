56ef56fd8c4a093f79e16bcd4e152a48ce086a67
Antonio Carlos <jr_acn@yahoo.com.br>
2021-05-03 11:00:37 -0300

    Update generate-ssh.sh

--- utils/generate-ssh.sh
+++ utils/generate-ssh.sh
@@ -7,3 +7,6 @@ eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 ssh-keygen -f ~/.ssh/id_rsa -y > ~/.ssh/id_rsa.pub
 xclip -sel clip < ~/.ssh/id_rsa.pub
+
+# Copy SSH to server
+# ssh-copy-id -i ~/.ssh/id_rsa.pub <user>@<server>
