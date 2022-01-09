69d3bfb6ab04dabf8da7a9cd4ce273a0ccfe41ad
Antonio Carlos <jr_acn@yahoo.com.br>
2022-01-09 19:30:56 -0300

    Update generate-ssh.sh

--- utils/generate-ssh.sh
+++ utils/generate-ssh.sh
@@ -4,9 +4,14 @@ ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 ssh-keygen -f ~/.ssh/id_rsa -y > ~/.ssh/id_rsa.pub
+
+## Copy Key if Linux
 sudo apt-get update
 sudo apt install xclip -y
 xclip -sel clip < ~/.ssh/id_rsa.pub
+
+## Copy key if wsdl
+cat ~/.ssh/id_rsa.pub | clip.exe
 
 # Copy SSH to server
 # ssh-copy-id -i ~/.ssh/id_rsa.pub <user>@<server>
